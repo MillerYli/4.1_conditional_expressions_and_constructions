db5ab6a30c60c33a8c27fb46644da6dc5abf9ba6
Yulia Miller <miller82.98@mail.ru>
2022-12-13 23:34:57 -0800

    Добавьте файлы проекта.

---
+++ 4.1. conditional expressions and constructions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyNamespase
+{
+    class Myclass
+    {
+        public static void Main(string[] args)
+        {
+            var myapples = 5;
+            var hisapples = 6;
+            var hispeares = 5;
+
+            var result = (myapples != hispeares) & (myapples < hisapples);
+
+            Console.WriteLine(result);
+
+            ////Задание 4.1.4
+            //string a, b;
+            //bool c = a != b;
+
+            ////Задание 4.1.5
+            //int A, B;
+            //double X, Y;
+
+            //bool C = (A < B) | (X > Y);
+
+            var numberone = 7;      //Значение переменной изменено
+            var numbertwo = 7;
+
+            if (numberone == numbertwo)
+            {
+                Console.WriteLine("Условие истино");
+            }
+            else
+            {
+                Console.WriteLine("Условие ложно");
+            }
+        }
+    }
+}
